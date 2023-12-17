3be0ef1362ba30861f6049957a8a0746e3403126
MAGS <120091142+Mercantec-MAGS@users.noreply.github.com>
2023-12-17 19:40:04 +0100

    Added Docker support and file for Blazor

--- Blazor/obj/Debug/net8.0/Blazor.AssemblyInfo.cs
+++ Blazor/obj/Debug/net8.0/Blazor.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("71b51827-3397-4607-bd01-c42760bf22d6")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
